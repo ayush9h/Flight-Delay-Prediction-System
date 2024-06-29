da3a3a83ae24db39472b0f3ae03dd620a244e839
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2024-06-29 18:17:08 +0530

    block fix

--- main.py
+++ main.py
@@ -7,62 +7,93 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from typing import Tuple
 from zenml import pipeline, step
+from zenml.logger import get_logger
+import logging
+
+
+logger = get_logger(__name__)
 
 @step
 def load_data() -> pd.DataFrame:
     """
     Load data from a CSV file into a pandas DataFrame.
     """
-    df = pd.read_csv('./flight_data.csv')
-    return df
+    try:
+        df = pd.read_csv('./flight_data.csv')
+        return df
+    except ValueError:
+        logger.error('Cannot Load the dataset')
+        raise ValueError
+    
 
 @step
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     Preprocess the data.
+
+    Args:
+        df:pd.DataFrame
     """
+
     # Drop unnecessary columns
-    df = df.drop(['time_hour', 'tailnum', 'year', 'sched_dep_time', 'sched_arr_time'], axis=1)
+    try:
+        df = df.drop(['time_hour', 'tailnum', 'year', 'sched_dep_time', 'sched_arr_time'], axis=1)
 
-    # Impute missing values for numeric columns
-    numeric_columns = ['dep_delay', 'arr_time', 'dep_time', 'air_time', 'arr_delay']
-    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-    df[numeric_columns] = imputer.fit_transform(df[numeric_columns])
+        # Impute missing values for numeric columns
+        numeric_columns = ['dep_delay', 'arr_time', 'dep_time', 'air_time', 'arr_delay']
+        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+        df[numeric_columns] = imputer.fit_transform(df[numeric_columns])
 
-    # Convert delay columns to integer
-    df['dep_delay'] = df['dep_delay'].astype(int)
-    df['arr_delay'] = df['arr_delay'].astype(int)
+        # Convert delay columns to integer
+        df['dep_delay'] = df['dep_delay'].astype(int)
+        df['arr_delay'] = df['arr_delay'].astype(int)
 
-    # Encode categorical variables
-    cat_cols = ['carrier', 'origin', 'dest']
-    label_encoders = {}
-    for col in cat_cols:
-        le = LabelEncoder() 
-        df[col] = le.fit_transform(df[col])
-        label_encoders[col] = le
+        # Encode categorical variables
+        cat_cols = ['carrier', 'origin', 'dest']
+        label_encoders = {}
+        for col in cat_cols:
+            le = LabelEncoder() 
+            df[col] = le.fit_transform(df[col])
+            label_encoders[col] = le
 
-    # Convert dep_delay to binary target variable
-    df['Target'] = np.where(df['dep_delay'] > 0, 1, 0)
-
-    return df
+        # Convert dep_delay to binary target variable
+        df['Target'] = np.where(df['dep_delay'] > 0, 1, 0)
+        return df
+    
+    except ValueError:
+        logger.error('Error during preprocessing')
+        raise ValueError
 
 @step
 def split_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     """
     Split the data into training and testing sets.
+
+    Args:
+        df: pd.DataFrame
     """
-    X = df[['carrier', 'origin', 'dest', 'distance', 'hour', 'day', 'month']]
-    y = df['Target']
+    try:
+        X = df[['carrier', 'origin', 'dest', 'distance', 'hour', 'day', 'month']]
+        y = df['Target']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    return X_train, X_test, y_train, y_test
+        return X_train, X_test, y_train, y_test
+
+    except Exception as e:
+        logging.error(e)
+        raise(e)
 
 @step
 def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> RandomForestClassifier:
     """
     Train a random forest classifier model.
     Other models analysis in the notebooks directory
+
+
+    Args:
+        X_train: pd.DataFrame
+        y_train: pd.Series
     """
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
@@ -72,12 +103,26 @@ def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> RandomForestClassi
 def evaluate_model(model: RandomForestClassifier, X_test: pd.DataFrame, y_test: pd.Series) -> Tuple[float, float, float]:
     """
     Evaluate the model and return accuracy, MSE, and F1 score.
+
+    Args:
+        model: RandomForestClassifier
+        X_test: pd.DataFrame
+        y_test: pd.Series
     """
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-    return accuracy, mse, f1
+    try:
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        logging.warning(f'Accuracy: {accuracy}')
+        logging.warning(f'Mean Squared Error: {mse}')
+        logging.warning(f'F1 Score: {f1}')
+        return accuracy, mse, f1
+    
+    except ValueError:
+        logger.error('Error during evaluation')
+        raise ValueError
+
 
 @pipeline
 def training_pipeline() -> Tuple[float, float, float]:
@@ -92,7 +137,6 @@ if __name__ == '__main__':
 
     '''
     In zenml dashboard, the following box metadata represents
-
     output0:accuracy_score, output1:mse, output2:f1_score
     '''
     training_pipeline()
